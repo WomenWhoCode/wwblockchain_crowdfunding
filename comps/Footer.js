bca47d664950d917532323886359f2fe76cefe01
akankshabh <akanksha.bhattachan@gmail.com>
2022-05-03 12:45:55 +0545

    dynamic nav, footer, layout

---
+++ comps/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return ( 
+        <div>Copyright @2022 JustFund
+             <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+        </div>
+    );
+   }
+   
+   export default Footer;
---
+++ comps/Navbar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return ( 
+       <nav>
+            <div className="logo">
+            <h1>Just Fund</h1>
+            </div>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/campaign"><a>Campaign Lists</a></Link>
+            <Link href="/campaign/new"><a>New Campaign</a></Link>
+       </nav>
+    );
+   }
+   
+   export default Navbar;
