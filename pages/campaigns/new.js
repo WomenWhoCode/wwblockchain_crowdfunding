8dcd4562756a744cacb7a7daab8957cf3644b7be
Lexi <akc1988@protonmail.com>
2022-05-13 11:39:49 -0400

    style changes minus new form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -63,24 +63,24 @@ const NewRequest = () => {
    // const result = await res.json();
    alert(`Create Campaign : Success`);
   }
-//  const NewCampaignForm = (
-//      <form className="container" onSubmit={handleSubmit}>
-//       <label htmlFor="minimum_contr_amt">Minimum Contribution Amount</label>
-//       <input type="text" id="minimum_contr_amt" name="minimum_contr_amt" required />   
-//       <label htmlFor="campaign_name">Campaign Name</label>
-//       <input type="text" id="campaign_name" name="campaign_name" required />  
-//      <label htmlFor="campaign_desc">Campaign Description</label>
-//       <textarea id="campaign_desc" name="campaign_desc" required />
-//       <label htmlFor="img_url">Image URL</label>
-//       <input type="text" id="img_url" name="img_url" required />
-//      
-//       <label htmlFor="target_amt">Target Amount</label>
-//       <input type="text" id="target_amt" name="target_amt" required />
-//       <br></br>
-//       <br></br>
-//       <button className="request-button" type="submit">Submit Request</button>
-//     </form>
-// );
+  const NewCampaignForm = (
+      <form className="container" onSubmit={handleSubmit}>
+       <label htmlFor="minimum_contr_amt">Minimum Contribution Amount</label>
+       <input type="text" id="minimum_contr_amt" name="minimum_contr_amt" required />   
+       <label htmlFor="campaign_name">Campaign Name</label>
+       <input type="text" id="campaign_name" name="campaign_name" required />  
+      <label htmlFor="campaign_desc">Campaign Description</label>
+       <textarea id="campaign_desc" name="campaign_desc" required />
+       <label htmlFor="img_url">Image URL</label>
+       <input type="text" id="img_url" name="img_url" required />
+      
+       <label htmlFor="target_amt">Target Amount</label>
+       <input type="text" id="target_amt" name="target_amt" required />
+       <br></br>
+       <br></br>
+       <button className="request-button" type="submit">Submit Request</button>
+     </form>
+ );
  const { connectWallet, address, error } = useWeb3();
  error ? console.log(error) : null;
    return ( 
@@ -105,9 +105,7 @@ const NewRequest = () => {
                           Connect Wallet
                 </Button>
        )}
-       <Input>
-       </Input>
-      {/*NewCampaignForm*/}
+      {NewCampaignForm}
      </div>
      </>
     );
